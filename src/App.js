27b44c69e04a71555d21d6dc64824a72bfa59793
Yussif <djiceman.gh@gmail.com>
2020-09-09 13:06:57 +0000

    Initial Setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Form from './components/Form';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Dev Career To-Do List</h1>
+      <Form></Form>
+    </div>
+  );
+}
+
+export default App;
